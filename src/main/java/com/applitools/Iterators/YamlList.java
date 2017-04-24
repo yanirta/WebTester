a81e630c09d9cb30a5cddb1d7d475ce2337d0fde
Yanir Taflev <yanir.taflev@applitools.com>
2017-04-24 19:34:45 +0300

    Adding Yaml list, still at work

---
+++ src/main/java/com/applitools/Iterators/YamlList.java
@@ -0,0 +1,12 @@
+package com.applitools.Iterators;
+
+import java.util.Iterator;
+
+/**
+ * Created by yanir on 15/09/2016.
+ */
+public class YamlList implements Iterable<String>  {
+    public Iterator<String> iterator() {
+        return null;
+    }
+}
